a578fde4582ea49d8954356a68e2d4b76192c9f2
sHKamil <shootekb@gmail.com>
2024-04-10 23:54:32 +0200

    assets added, welcome.view created

--- application/App/Views/welcome.view.php
+++ application/App/Views/welcome.view.php
@@ -7,9 +7,73 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <link rel="stylesheet" href="public/assets/welcome.css">
+    <title>MEBEVERIN</title>
 </head>
 <body>
-    <h1>Welcome</h1>
+    
+    <main>
+        <div class="logo">
+            <span class="fc-orange-100">MEB</span><span class="fc-dark">EVERIN</span>
+        </div>
+        <div class="bar bg-orange-100"></div>
+        <div class="w-title fw-regular">
+            Zapraszamy do serwisu edukacyjnego <span class="fw-bold"> dla lekarzy! </span>
+        </div>
+
+        <div class="cards">
+            <div class="card">
+                <div class="icon bg-gradient-green">
+
+                </div>
+                <div class="inner-card">
+
+                    <div class="c-title fw-bold">
+                        Materiały video
+                    </div>
+                    <div class="bar bg-green-100"></div>
+                    <div class="content">
+                        Opisz, co użytkownik może obejrzeć w Twoim serwisie. 
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="icon bg-gradient-orange">
+
+                </div>
+                <div class="inner-card">
+                    <div class="c-title fw-bold">
+                        Artykuły
+                    </div>
+                    <div class="bar bg-orange-100"></div>
+                    <div class="content">
+                        Zaproś odbiorcę do Twojej wirtualnej biblioteki.
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="icon bg-gradient-purple">
+
+                </div>
+                <div class="inner-card">
+
+                    <div class="c-title fw-bold">
+                        E-detaling
+                    </div>
+                    <div class="bar bg-purple-100"></div>
+                    <div class="content">
+                        Jednym zdaniem określ, czego odbiorca może dowiedzieć się o Twoim produkcie.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <form>
+            <input class="pwz" type="text" name="pwz" placeholder="PWZ">
+            <input type="submit">
+        </form>
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
